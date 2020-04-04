45258703945bafa851fddc81256f7c7a4e53ea0a
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-04-04 16:46:16 -0500

    Can read freq and talk to the radio

---
+++ RigBus/Program.cs
@@ -0,0 +1,24 @@
+﻿using HambusCommonLibrary;
+using System;
+
+namespace RigBus
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      CommPortConfig portConf = new CommPortConfig();
+      portConf.BaudRate = 57600;
+      portConf.BaudRate = 8;
+      portConf.StopBits = "one";
+      portConf.Parity = "none";
+      portConf.PortName = "com21";
+      portConf.DisplayName = "PowerSDR";
+      portConf.Handshake = "none";
+
+      var kenwood = new Kenwood();
+      kenwood.OpenPort(portConf);
+      Console.ReadKey();
+    }
+  }
+}
