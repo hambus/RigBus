35a033320ac200a65fcaa004796a6dfece436064
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-08-30 14:51:16 -0500

    skelton start that will compile

---
+++ VirtualRigBus/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine;
+using CoreHambusCommonLibrary.Model;
+using HambusCommonLibrary;
+using HamBusCommonStd.Model;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace RigBus
+{
+  public class Options
+  {
+    [Option('p', "port", Required = false, HelpText = "HTTP Port that Master Bus will listen")]
+    public int? Port { get; set; } = 7300;
+
+    [Option('h', "host", Required = false, HelpText = "Name of HTTP host that Master Bus will listen")]
+    public string? Host { get; set; } = "localhost";
+
+    [Option('n', "name", Required = true, HelpText = "Name of the instance.")]
+    public string? Name { get; set; }
+
+  }
+  class Program
+  {
+    RigBusMain? rigMain = null;
+    static async Task Main(string[] args)
+    {
+      var prog = new Program();
+      await prog.Run(args);
+    }
+
+    async Task Run(string[] args)
+    {
+      rigMain = new RigBusMain();
+      Parser.Default.ParseArguments<Options>(args)
+        .WithParsed(RunOptions)
+        .WithNotParsed(HandleParseError);
+
+      await rigMain.Run();
+
+      while (true) Thread.Sleep(100000);
+    }
+    void RunOptions(Options opts)
+    {
+      if (rigMain == null)
+        throw new NullReferenceException("RigMain");
+      if (opts.Name != null)
+        Bus.Name = opts.Name;
+      if (opts.Host != null)
+        rigMain!.MasterHost = opts.Host;
+      if (opts.Port != null)
+        rigMain!.MasterPort = Convert.ToInt32(opts.Port);
+    }
+    void HandleParseError(IEnumerable<Error> errs)
+    {
+      throw new Exception("Invalid Args");
+    }
+  }
+}
---
+++ VirtualRigBus/RigBusMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.IO.Ports;
+using System.Threading.Tasks;
+using CoreHambusCommonLibrary.Model;
+using CoreHambusCommonLibrary.Networking;
+using HamBusCommonStd;
+using Microsoft.AspNetCore.SignalR.Client;
+
+
+
+namespace RigBus
+{
+  public class RigBusMain: Bus
+  {
+    public KenwoodRig? rig { get; set; }
+    private SigRConnection? sigRConn;
+
+    public async Task Run()
+    {
+      sigRConn = SigRConnection.Instance;
+
+      sigRConn.RigState__.Subscribe<RigState>(state => OnStateChange(state));
+      //sigRConn.LockModel__.Subscribe<LockModel>(locker => OnLockChange(locker));
+
+      rig = new KenwoodRig();
+
+      connection = await sigRConn.StartConnection($"http://{MasterHost}:{MasterPort}/masterbus");
+
+      List<string>? groupList = new List<string>();
+      groupList.Add("radio");
+      groupList.Add("logging");
+      groupList.Add("virtual");
+      var ports = GetAvailableSerialPort();
+      
+      Login(Name, groupList, ports);
+    }
+
+    //private void OnLockChange(LockModel locker)
+    //{
+
+    //  if (locker == null || rig == null) return;
+    //  rig.IsStateLocked = locker.IsStateLocked;
+    //}
+
+    public async void Login(string name, List<string>? group, List<string>? ports)
+    {
+      rig!.Name = name;
+      try
+      {
+        await connection.InvokeAsync("Login", name, group);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Error: {ex.Message}");
+      }
+    }
+
+
+    private void OnStateChange(RigState state)
+    {
+      rig!.PausePolling = true;
+      Console.WriteLine($"test of serial #{ state.SerialNum}");
+      rig!.SetStateFromBus(state);
+      rig!.PausePolling = false;
+
+    }
+
+    private List<string> GetAvailableSerialPort()
+    {
+      List<string> list = new List<string>(SerialPort.GetPortNames());
+      return list;
+
+    }
+  }
+}
